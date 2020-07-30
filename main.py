8b7ef39c1fc0e5fb42c5dcfae4dc7e8d79a2393f
Thiyasizwe Kubeka <thiyasizwekubeka@gmail.com>
2020-07-30 14:13:21 +0200

    first commit

--- main.py
+++ main.py
@@ -4,7 +4,11 @@
 
 
 #Function_2
-
+def five_num_summary(items):
+    five_num_sum = np.percentile(items,[0, 25, 50, 75, 100])
+    dict = {'min': five_num_sum[0],'Q1':five_num_sum[1],'Median':five_num_sum[2],'Q3':five_num_sum[3],'Max':five_num_sum[4]}
+    
+    return dict
 
 
 #Function_3
@@ -14,7 +18,7 @@
 #Function_4
 
 
-d
+
 #Function_5
 
 
