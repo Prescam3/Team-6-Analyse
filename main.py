b5cad41a0149995bd60bf09b0893dfb5ca22ef53
Prescam3 <60913826+Prescam3@users.noreply.github.com>
2020-07-30 10:11:19 +0200

    my first commit

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 #Function_1
+### START FUNCTION 1
+def dictionary_of_metrics(items):
+    """A function that calculates the dictionary of metrics including mean,
+    median, standard deviation, variance, minimum value, and a maximum value from a list
+    """
+    mean = np.mean(items).round(2)
+    median = np.median(items).round(2)
+    std = np.std(items, ddof=1).round(2)
+    var = np.var(items, ddof=1).round(2)
+    min = np.min(items).round(2)
+    max = np.max(items).round(2)
+ 
+    
+    #dictionary of metrics
+    dict = {'mean':mean, 'median':median, 'std':std, 'var':var, 'min':min, 'max':max, }
+    
+    
+    return  dict
 
+### END FUNCTION
 
 
 
