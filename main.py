515104f3b23d75478cb73de89f4297ded9b0a1b7
Prescam3 <60913826+Prescam3@users.noreply.github.com>
2020-08-03 10:58:04 +0200

    Revert "first commit"

--- main.py
+++ main.py
@@ -99,11 +99,7 @@ def dictionary_of_metrics(items):
 
 
 #Function_2
-def five_num_summary(items):
-    five_num_sum = np.percentile(items,[0, 25, 50, 75, 100])
-    dict = {'min': five_num_sum[0],'Q1':five_num_sum[1],'Median':five_num_sum[2],'Q3':five_num_sum[3],'Max':five_num_sum[4]}
-    
-    return dict
+
 
 
 #Function_3
